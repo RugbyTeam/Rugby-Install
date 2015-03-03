97264020e3039485f5a357631636c320a5c41ff7
Alex Gonzales <agonz056@ucr.edu>
2015-03-03 18:05:32 -0500

    running whole script as sudo

--- install.sh
+++ install.sh
@@ -1,32 +1,41 @@
 #! /bin/bash
+{
+    # clear any previous sudo permission
+    sudo -k
+
+    # run inside sudo
+    sudo sh <<SCRIPT
+
+# update sources
+apt-get update
 
 # install vagrant and virtualbox
 # install dpkg and virtualbox
-sudo apt-get install dpkg-dev virtualbox-dkms
+apt-get install -y dpkg-dev virtualbox-dkms
 
 # install vagrant
 VAGRANT_URL="https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb"
 VAGRANT_PACKAGE="vagrant.deb"
 
 wget "$VAGRANT_URL" -v -O "$VAGRANT_PACKAGE"
-sudo dpkg -i "$VAGRANT_PACKAGE"
+dpkg -i "$VAGRANT_PACKAGE"
 rm "$VAGRANT_PACKAGE"
 
 # install linux headers
 KERNEL_VERSION=$(uname -r)
-sudo apt-get install "linux-headers-$KERNEL_VERSION"
+apt-get install "linux-headers-$KERNEL_VERSION"
 
-sudo dpkg-reconfigure virtualbox-dkms
+dpkg-reconfigure virtualbox-dkms
 
 # install python-dev and pip
-sudo apt-get install python-dev
+apt-get install python-dev
 
 # install pip
 PIP_URL="https://bootstrap.pypa.io/get-pip.py"
-wget -qO- "$PIP_URL" | sudo python -
+wget -qO- "$PIP_URL" | python -
 
 # install virtualenv
-sudo pip install virtualenv
+pip install virtualenv
 
 RUGBY_BASE_PATH="/opt"
 RUGBY_APP="$RUGBY_BASE_PATH/Rugby-App"
@@ -35,16 +44,16 @@ RUGBY_SERVER="$RUGBY_APP/Rugby-Server"
 RUGBY_TESTS="$RUGBY_APP/Rugby-Tests"
 RUGBY_PLAYBOOKS="$RUGBY_APP/Rugby-Playbooks"
 
-sudo mkdir -p "$RUGBY_APP"
+mkdir -p "$RUGBY_APP"
 
 # install git
-sudo apt-get install git
+apt-get install git
 
 # Clone project repos
-sudo git clone https://github.com/RugbyTeam/Rugby "$RUGBY_CORE"
-sudo git clone https://github.com/RugbyTeam/Rugby-Server "$RUGBY_SERVER"
-sudo git clone https://github.com/RugbyTeam/Rugby-Tests "$RUGBY_TESTS"
-sudo git clone https://github.com/RugbyTeam/Rugby-Playbooks "$RUGBY_PLAYBOOKS"
+git clone https://github.com/RugbyTeam/Rugby "$RUGBY_CORE"
+git clone https://github.com/RugbyTeam/Rugby-Server "$RUGBY_SERVER"
+git clone https://github.com/RugbyTeam/Rugby-Tests "$RUGBY_TESTS"
+git clone https://github.com/RugbyTeam/Rugby-Playbooks "$RUGBY_PLAYBOOKS"
 
 # create and activate virtualenv
 virtualenv "$RUGBY_CORE/venv"
@@ -60,3 +69,5 @@ TO_PATCH="$PYTHONPATH/venv/lib/python2.7/site-packages/vagrant/__init__.py"
 PATCH="$PYTHONPATH/patches/vagrant_quiet_logging_issue.patch"
 patch "$TO_PATCH" < "$PATCH"
 
+SCRIPT
+}
